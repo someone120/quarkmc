fe5cc06a1476171069f3f0643727edd43073fcd7
someone <renmou533@gmail.com>
2020-03-18 20:50:50 +0800

    remove the node_modules

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-import ws from "nodejs-websocket";
+var ws = require("nodejs-websocket");
+var plugins=require("plugin.js")
+var server = ws.createServer(function(conn){
+    conn.on("text", function (str) {
+        
+    })
+}
+).listen(8001)
+console.log("[server info]已启动服务器，等待连接");
---
+++ plugin.js
@@ -0,0 +1,5 @@
+var sqlite3=require("sqlite3");
+var fs = require('fs');
+var db=new sqlite3.Database("database.db",function(){
+
+})
