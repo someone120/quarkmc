02f4fb324aa5da4af0afaa0176701a314732fa10
someone <renmou533@gmail.com>
2020-03-19 10:46:37 +0800

    sometime you must to know, A good write habit is n

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 var ws = require("nodejs-websocket");
-var plugins=require("plugin.js")
-var app = require ("sys/app.js")
-var server = ws.createServer(function(conn){
-    conn.on("text", function (str) {
-        
-    })
-    conn.on("connect",function(){
-        app.getByComm()
-    })
-}
-).listen(8001)
+var plugins = require("plugin.js");
+var app = require("sys/app.js");
+var server = ws
+  .createServer(function(conn) {
+    conn.on("text", function(str) {});
+    conn.on("connect", function() {
+      app.getByComm();
+    });
+  })
+  .listen(8001);
 console.log("[server info]已启动服务器，等待连接");
--- plugin.js
+++ plugin.js
@@ -1,5 +1,3 @@
-var sqlite3=require("sqlite3");
-var fs = require('fs');
-var db=new sqlite3.Database("database.db",function(){
-
-})
+var sqlite3 = require("sqlite3");
+var fs = require("fs");
+var db = new sqlite3.Database("database.db", function() {});
