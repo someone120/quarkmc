10badb86c21475b5bf7bcac89667f2476cabb468
someone <renmou533@gmail.com>
2020-03-25 10:13:55 +0800

    fix the bugs.

--- plugin.js
+++ plugin.js
@@ -12,26 +12,33 @@ var db = new sqlite3.Database(file);
 if (!fs.existsSync(file)) {
     console.log("[Plugins info]Creating db file!");
     fs.openSync(file, "w");
-    db.run("CREATE TABLE PLUGINS(" + "COMMON  CHAR(128)," + "NAME  CHAR(32) NOT NULL);")
+    db.run(
+        "CREATE TABLE PLUGINS(" +
+            "COMMON  CHAR(128)," +
+            "NAME  CHAR(32) NOT NULL);"
+    );
 }
 
 function copyFile(src, dist) {
     fs.createReadStream(src).pipe(fs.createWriteStream(dist));
 }
 //初始化完毕
-exports.getTheResult = function (str, conn) {
+exports.getTheResult = function(str, conn) {
     console.log("[Plugin info] get Result:" + str);
     if (JSON.parse(str)["header"]["messagePurpose"] == "event") {
         str = JSON.parse(str)["body"]["properties"]["Message"];
-        db.all("select NAME,COMMON from PLUGINS where COMMON LIKE '" + str + "%';", function (err, row) {
-            //读有什么导入的命令和插件
-            console.log(JSON.stringify(row));
-            if (row[0] != undefined) {
-                let plugin = require("./plugins/" + row[0]["NAME"]);
-                let result = plugin.getForChat(str, conn);
-                console.log(result);
+        db.all(
+            "select NAME,COMMON from PLUGINS where COMMON LIKE '" + str + "%';",
+            function(err, row) {
+                //读有什么导入的命令和插件
+                console.log(JSON.stringify(row));
+                if (row[0] != undefined) {
+                    let plugin = require("./plugins/" + row[0]["NAME"]);
+                    let result = plugin.getForChat(str, conn);
+                    console.log(result);
+                }
             }
-        });
+        );
         //callback("say hello", "");
     }
 };
@@ -39,27 +46,33 @@ exports.getTheResult = function (str, conn) {
 var arguments = process.argv; //获取参数
 function install(path) {
     console.log("test");
-    var plugin = require(path); //导入文件
-    var commons = plugin.getAllComm() //获取可执行的命令
+    var plugin = require("./" + path); //导入文件
+    var commons = plugin.getAllComm(); //获取可执行的命令
     copyFile(path, "./plugins/" + path); //复制到plugins文件夹
-    var name = path.split("/")[path.split("/").length - 1]
-    var sql = "INSERT INTO 'PLUGINS' SELECT '" + commons[0] + "' AS 'COMMON','" + name + "' AS 'NAME'"
+    var name = path.split("/")[path.split("/").length - 1];
+    var sql =
+        "INSERT INTO 'PLUGINS' SELECT '" +
+        commons[0] +
+        "' AS 'COMMON','" +
+        name +
+        "' AS 'NAME'";
     for (i = 1; i < commons.length; i++) {
-        sql += "UNION SELECT '" + commons[i] + "','" + name + "'"
+        sql += "UNION SELECT '" + commons[i] + "','" + name + "'";
     }
-    sql += ";"
+    sql += ";";
     var sql1 = db.prepare(sql);
-    sql1.run()
+    sql1.run();
 }
 console.log(arguments);
 console.log(__dirname);
 if (arguments[1] == __dirname + "/plugin.js") {
     //判断是否直接调用
     if (arguments[2] == "install") {
-        if (arguments[3] != undefined) {//防止憨批
-            install(arguments[3])
+        if (arguments[3] != undefined) {
+            //防止憨批
+            install(arguments[3]);
         }
     }
 }
 
-exports.install = install
+exports.install = install;
